7301a5bc3bdbc1fa88e5531d60bd01aafa82b78a
Joe McIlvain <joe.eli.mac@gmail.com>
2013-12-14 11:44:55 -0500

    Added Contain::Guard

--- lib/contain.rb
+++ lib/contain.rb
@@ -26,4 +26,27 @@ module Contain
     end
   end
   
+  
+  module Guard
+    def self.warn_about obj
+      warn "WARNING"
+      warn "#{obj} is intended only for use in a Contain::Component."
+      warn "Please use Contain::Host#contain instead of #extend or #include."
+      warn ""
+    end
+    
+    def self.guard mod
+      mod.send :define_method, :extended do |obj|
+        ::Contain::Guard.warn_about self unless obj.is_a? ::Contain::Component
+      end
+      mod.send :define_method, :included do |_|
+        ::Contain::Guard.warn_about self
+      end
+    end
+    
+    def self.included mod
+      guard mod.singleton_class
+    end
+  end
+  
 end
