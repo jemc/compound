fbf904821b0967cefbbd874cfc54f33fc5b82007
Joe McIlvain <joe.eli.mac@gmail.com>
2013-12-19 01:13:22 -0500

    Add docstrings for Host

--- lib/compound/host.rb
+++ lib/compound/host.rb
@@ -1,7 +1,13 @@
 
 module Compound
   
+  # Include this module in a class to gain the ability to host Parts,
+  # which each embody a module, appearing to an outside object as if
+  # the modules themselves were mixed in, while maintaining separation
+  # among the modules.  Refer to the README for more information
   module Host
+    
+    # Internalize a new Part embodying the given module
     def compound mod
       @_compound_parts ||= []
       uncompound mod
@@ -9,21 +15,26 @@ module Compound
       mod.compounded(self) if mod.respond_to? :compounded
     end
     
+    # Remove the Part associated with the given module
     def uncompound mod
       @_compound_parts.reject! { |part| part.is_a? mod }
     end
     
+    # Forward an undefined method if it is in one of the Parts
     def method_missing sym, *args, &block
       component = @_compound_parts && 
                   @_compound_parts.detect { |obj| obj.respond_to? sym }
       component ? (component.send sym, *args, &block) : super
     end
     
+    # Pretend to also respond_to methods in the Parts as well as the Host
     def respond_to? sym
       super || !!(@_compound_parts && 
                   @_compound_parts.detect { |obj| obj.respond_to? sym })
     end
     
+    # Return the Method object associated with the symbol, 
+    # even if it is in one of the Parts and not the Host
     def method sym
       return super if methods.include? sym
       component = @_compound_parts && 
@@ -31,6 +42,7 @@ module Compound
       component ? component.method(sym) :
         raise(NameError, "undefined method `#{sym}' for object `#{self}'")
     end
+    
   end
   
 end
